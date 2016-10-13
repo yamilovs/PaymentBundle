a1ea1819687c47516d368f8b75126461cb1e9321
Yamilov Stepan <yamilovs@gmail.com>
2016-10-13 12:18:01 +0500

    Change configuration root name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('payment');
+        $rootNode = $treeBuilder->root('yamilovs_payment');
 
         $rootNode
             ->children()
