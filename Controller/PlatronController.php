ac2c6c4e1671827e41cb33d11c011b71458974d8
Yamilov Stepan <yamilovs@gmail.com>
2016-10-14 17:25:13 +0500

    Move bundle routing to separate yml files

--- Controller/PlatronController.php
+++ Controller/PlatronController.php
@@ -3,106 +3,80 @@
 namespace Yamilovs\PaymentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Yamilovs\PaymentBundle\Component\HttpFoundation\XmlResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Yamilovs\PaymentBundle\Component\HttpFoundation\XmlResponse;
 use Yamilovs\PaymentBundle\Manager\PaymentServicePlatron;
 
-/**
- * Class TourCardController
- * @Route("/platron")
- * @package Yamilovs\PaymentBundle\Controller
- */
 class PlatronController extends Controller
 {
     /**
      * Проверка возможности платежа
      *
-     * @Route("/check", name="payment_platron_check")
-     * @Method({"POST"})
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return XmlResponse
      */
     public function checkAction(Request $request)
     {
-        /** @var PaymentServicePlatron $platron */
-        $manager = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
-        $params = $request->request->all();
-        // check signature
-        $data = $manager->checkPayment($this->generateUrl('payment_platron_check'), $params);
+        /** @var PaymentServicePlatron $manager */
+        $manager    = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
+        $params     = $request->request->all();
+        $data       = $manager->checkPayment($this->generateUrl('yamilovs_payment_platron_check'), $params);
         return new XmlResponse($data);
     }
 
     /**
      * Сообщение о результате платежа
      *
-     * @Route("/result", name="payment_platron_result")
-     * @Method({"POST"})
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return XmlResponse
      */
     public function resultAction(Request $request)
     {
-        /** @var PaymentServicePlatron $platron */
-        $manager = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
-        $params = $request->request->all();
-        // check signature
-        $data = $manager->resultPayment($this->generateUrl('payment_platron_result'), $params);
+        /** @var PaymentServicePlatron $manager */
+        $manager    = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
+        $params     = $request->request->all();
+        $data       = $manager->resultPayment($this->generateUrl('yamilovs_payment_platron_result'), $params);
         return new XmlResponse($data);
     }
 
     /**
      * Сообщение о результате платежа
      *
-     * @Route("/result", name="payment_platron_refund")
-     * @Method({"POST"})
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return XmlResponse
      */
     public function refundAction(Request $request)
     {
-        /** @var PaymentServicePlatron $platron */
-        $manager = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
-        $params = $request->request->all();
-        // check signature
-        $data = $manager->resultPayment($this->generateUrl('payment_platron_refund'), $params);
+        /** @var PaymentServicePlatron $manager */
+        $manager    = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
+        $params     = $request->request->all();
+        $data       = $manager->resultPayment($this->generateUrl('yamilovs_payment_platron_refund'), $params);
         return new XmlResponse($data);
     }
 
     /**
      * Url на который перенаправляется пользователь при удачном платеже
      *
-     * @Route("/success", name="payment_platron_success")
-     * @Method({"POST"})
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function successAction(Request $request)
     {
-        /** @var PaymentServicePlatron $platron */
-        $manager = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
-        $params = $request->request->all();
-        // check signature
-        $manager->successPayment($this->generateUrl('payment_platron_success'), $params);
-        return $this->render('YamilovsPaymentBundle:Default:index.html.twig');
+        /** @var PaymentServicePlatron $manager */
+        $manager    = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
+        $params     = $request->request->all();
+        $manager->successPayment($this->generateUrl('yamilovs_payment_platron_success'), $params);
     }
 
     /**
      * Url на который перенаправляется пользователь при неудачном платеже
      *
-     * @Route("/failure", name="payment_platron_failure")
-     * @Method({"POST"})
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function failureAction(Request $request)
     {
+        /** @var PaymentServicePlatron $manager */
         $manager = $this->get('yamilovs.payment.factory')->get(PaymentServicePlatron::ALIAS);
         $params = $request->request->all();
-        // check signature
-        $manager->failurePayment($this->generateUrl('payment_platron_failure'), $params);
-        return $this->render('YamilovsPaymentBundle:Default:index.html.twig');
+        $manager->failurePayment($this->generateUrl('yamilovs_payment_platron_failure'), $params);
     }
-
 }
