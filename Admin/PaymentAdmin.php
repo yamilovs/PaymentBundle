fa1e034a60a2e794ec225ff3290247ac36f1f526
Yamilov Stepan <yamilovs@gmail.com>
2016-10-14 14:49:51 +0500

    Added translations for Sonata Admin class and a little cleaning it.

--- Admin/PaymentAdmin.php
+++ Admin/PaymentAdmin.php
@@ -2,26 +2,19 @@
 
 namespace Yamilovs\PaymentBundle\Admin;
 
-use Imagine\Filter\Basic\Copy;
-use ProfiTravel\NasheTravelBundle\Entity\Article;
-use ProfiTravel\NasheTravelBundle\Form\Type\CkeditorType;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\CoreBundle\Form\Type\DateTimePickerType;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Yamilovs\PaymentBundle\Entity\Payment;
 use Yamilovs\PaymentBundle\Manager\PaymentFactory;
 
-
 class PaymentAdmin extends AbstractAdmin
 {
-
     protected $payments;
 
     public function setPayments(PaymentFactory $paymentFactory)
@@ -29,126 +22,105 @@ class PaymentAdmin extends AbstractAdmin
         $this->payments = $paymentFactory->getPayments();
     }
 
-    // setup the default sort column and order
     protected $datagridValues = array(
-        '_sort_order' => 'DESC',
-        '_sort_by' => 'createdAt'
+        '_sort_order'   => 'DESC',
+        '_sort_by'      => 'createdAt'
     );
 
-    // Fields to be shown on create/edit forms
     protected function configureFormFields(FormMapper $formMapper)
     {
         /** @var Payment $entity */
         $entity = $this->getSubject();
         $isNew = ($entity->getId()) ? false : true;
 
-        $formMapper
-            ->with('Основное')
-                ->add('status', ChoiceType::class, [
-                    'label'     => 'Статус',
-                    'choices'   => Payment::getStatuses(),
-                    'required'  => true,
-                ])
-                ->add('paymentType', ChoiceType::class, [
-                    'label'     => 'Платежная система',
-                    'choices'   => $this->payments,
-                    'required'  => true,
-                ])
-                ->add('paymentId', TextType::class, [
-                    'label'     => 'Id платежа во внешней системе',
-                    'required'  => true,
-                ])
-                ->add('purchase', null, [
-                    'label' => 'Платеж',
-                ])
-                ->add('invoiceSum', NumberType::class, [
-                    'label'     => 'Сумма',
-                    'required'  => true,
-                ])
-                ->add('paidSum', NumberType::class, [
-                    'label'     => 'Сумма',
-                    'required'  => true,
-                ])
-            ->end()
-        ;
-
         if (!$isNew) {
             $formMapper
-                ->with('Информация')
-                ->add('created_at', DateTimePickerType::class, array(
-                    'label' => 'Дата создания статьи',
-                    'widget' => 'single_text',
-                    'format' => 'dd MMMM YYYY в H:m:s',
-                    'required' => false,
-                    'disabled' => true,
-                ))
-                ->add('updated_at', DateTimePickerType::class, array(
-                    'label' => 'Дата обновления статьи',
-                    'widget' => 'single_text',
-                    'format' => 'dd MMMM YYYY в H:m:s',
-                    'required' => false,
-                    'disabled' => true,
-                ))
+                ->with('yamilovs.payment.admin.with.information')
+                    ->add('created_at', DateTimePickerType::class, array(
+                        'label' => 'yamilovs.payment.admin.label.created_at',
+                        'widget' => 'single_text',
+                        'format' => 'dd MMMM YYYY в H:m:s',
+                        'required' => false,
+                        'disabled' => true,
+                    ))
+                    ->add('updated_at', DateTimePickerType::class, array(
+                        'label' => 'yamilovs.payment.admin.label.updated_at',
+                        'widget' => 'single_text',
+                        'format' => 'dd MMMM YYYY в H:m:s',
+                        'required' => false,
+                        'disabled' => true,
+                    ))
                 ->end()
             ;
         }
+
+        $formMapper
+            ->with('yamilovs.payment.admin.with.data')
+                ->add('status', ChoiceType::class, [
+                    'label' => 'yamilovs.payment.admin.label.status',
+                    'choices' => Payment::getStatuses(),
+                ])
+                ->add('paymentType', ChoiceType::class, [
+                    'label' => 'yamilovs.payment.admin.label.payment_type',
+                    'choices' => $this->payments,
+                ])
+                ->add('paymentId', TextType::class, [
+                    'label' => 'yamilovs.payment.admin.label.payment_id',
+                ])
+                ->add('purchase', null, [
+                    'label' => 'yamilovs.payment.admin.label.purchase',
+                    'required' => true,
+                ])
+                ->add('invoiceSum', IntegerType::class, [
+                    'label' => 'yamilovs.payment.admin.label.invoice_sum',
+                ])
+                ->add('paidSum', IntegerType::class, [
+                    'label' => 'yamilovs.payment.admin.label.paid_sum',
+                ])
+            ->end()
+        ;
     }
 
-    // Fields to be shown on filter forms
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
             ->add('id')
-            ->add('paymentType',
-                'doctrine_orm_choice',
-                ['label' => 'Статус'],
-                'choice' ,
-                ['choices' => $this->payments]
-            )
-            ->add('status',
-                'doctrine_orm_choice',
-                ['label' => 'Статус'],
-                'choice' ,
-                ['choices' => Payment::getStatuses()]
-            )
+            ->add('paymentType', 'doctrine_orm_choice', ['label' => 'yamilovs.payment.admin.label.payment_type'], 'choice', ['choices' => $this->payments])
+            ->add('status', 'doctrine_orm_choice', ['label' => 'yamilovs.payment.admin.label.status'], 'choice', ['choices' => Payment::getStatuses()])
         ;
     }
 
-    // Fields to be shown on lists
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
             ->addIdentifier('id')
             ->add('status', 'choice', [
-                'label'     => 'Статус',
-                'choices'   => Payment::getStatuses(),
-                'required'  => true,
+                'label' => 'yamilovs.payment.admin.label.status',
+                'choices' => Payment::getStatuses(),
             ])
             ->add('paymentType', 'choice', [
-                'label'     => 'Платежная система',
-                'choices'   => $this->payments,
-                'required'  => true,
+                'label' => 'yamilovs.payment.admin.label.payment_type',
+                'choices' => $this->payments,
             ])
             ->add('paymentId', null, [
-                'label'     => 'Id платежа во внешней системе',
-                'required'  => true,
+                'label' => 'yamilovs.payment.admin.label.payment_id',
             ])
             ->add('purchase', null, [
-                'label' => 'Платеж',
-                'required' => false,
-                'multiple' => true,
-                'by_reference' => false,
+                'label' => 'yamilovs.payment.admin.label.purchase',
             ])
             ->add('invoiceSum', null, [
-                'label'     => 'Сумма счета',
-                'required'  => true,
+                'label' => 'yamilovs.payment.admin.label.invoice_sum',
             ])
             ->add('paidSum', null, [
-                'label'     => 'Сумма оплаты',
-                'required'  => true,
+                'label' => 'yamilovs.payment.admin.label.paid_sum',
+            ])
+            ->add('createdAt', null, [
+                'label' => 'yamilovs.payment.admin.label.created_at',
+            ])
+            ->add('updatedAt', null, [
+                'label' => 'yamilovs.payment.admin.label.updated_at',
             ])
             ->add('_action', 'actions', array(
-                'label' => 'Действия',
                 'actions' => array(
                     'edit' => array(),
                     'delete' => array(),
@@ -157,10 +129,8 @@ class PaymentAdmin extends AbstractAdmin
         ;
     }
 
-    // Field to be exported into file
     public function getExportFields()
     {
-        return array('id', 'paymentType', 'paymentId', 'status', 'sum', 'createdAt', 'updatedAt');
+        return array('id', 'status', 'paymentType', 'paymentId', 'invoiceSum', 'paidSum', 'createdAt', 'updatedAt');
     }
-
 }
