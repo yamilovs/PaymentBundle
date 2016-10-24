52cff79655106fa06792a598ed0a11a4e30bfc1f
Stepan Yamilov <yamilovs@gmail.com>
2016-10-24 12:02:08 +0500

    Fix array keys in exception message

--- Manager/AbstractPaymentService.php
+++ Manager/AbstractPaymentService.php
@@ -50,7 +50,7 @@ abstract class AbstractPaymentService
         $missingParameters = array_diff($requiredParameters, array_keys($parameters));
         if ($missingParameters) {
             throw new PaymentServiceInvalidArgumentException("Some required parameters does not exists. Has: " . implode(", ", array_keys($parameters)) . ". 
-            Also need: " . implode(", ", array_keys($missingParameters)));
+            Also need: " . implode(", ", $missingParameters));
         }
     }
 
